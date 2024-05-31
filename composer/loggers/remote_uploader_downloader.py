9fdfc82b55525afda7d027eb0a9e892160de9ac1
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2024-05-30 22:27:55 -0700

    update

--- composer/loggers/remote_uploader_downloader.py
+++ composer/loggers/remote_uploader_downloader.py
@@ -574,7 +574,10 @@ class RemoteUploaderDownloader(LoggerDestination):
         log.warning('In UD: here 4')
         # Then, ensure all workers have finished all uploads
         for worker in self._workers:
-            worker.join()
+            worker.join(timeout=30)
+            if worker.is_alive():
+                log.warning(f'Worker {worker} did not exit in time and is still alive.')
+
         log.warning('In UD: here 5')
 
         # Clean up the tempdir
